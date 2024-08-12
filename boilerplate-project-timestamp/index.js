01315a37d6d56b9ee30d3f5bb93762aa73ceb184
Ebuka <cpdokoye7@gmail.com>
2024-08-12 11:58:16 +0300

    C:/Program Files/Git/api route

--- boilerplate-project-timestamp/index.js
+++ boilerplate-project-timestamp/index.js
@@ -19,10 +19,11 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/", (req, res) => {
+  const dateNow = Date.now();
+  res.json({unix: dateNow, utc: new Date(dateNow).toUTCString()});
 });
+
 
 
 
