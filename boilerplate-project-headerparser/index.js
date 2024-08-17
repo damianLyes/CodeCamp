e609268a9d1e44afe253dc11f1134e1cdc9ef57d
Ebuka <cpdokoye7@gmail.com>
2024-08-17 14:07:50 +0300

    headerparser solutionn

--- boilerplate-project-headerparser/index.js
+++ boilerplate-project-headerparser/index.js
@@ -20,8 +20,11 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.ip;
+  const {'user-agent': software, 'accept-language': language} = req.headers;
+  
+  res.json({ipaddress, language, software});
 });
 
 // listen for requests :)
